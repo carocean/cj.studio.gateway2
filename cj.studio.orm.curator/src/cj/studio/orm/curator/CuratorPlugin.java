161021b9dbc0e81be25ca3292bb9e59d8ed7b1d9
cj <cj@ss.com>
2020-05-04 23:48:50 +0800

    2.4.3 add curator

--- cj.studio.orm.curator/src/cj/studio/orm/curator/CuratorPlugin.java
+++ cj.studio.orm.curator/src/cj/studio/orm/curator/CuratorPlugin.java
@@ -1,9 +1,11 @@
 package cj.studio.orm.curator;
 
 import cj.studio.ecm.CJSystem;
+import cj.studio.ecm.EcmException;
 import cj.studio.ecm.IAssemblyContext;
 import cj.studio.ecm.IChipPlugin;
 import cj.studio.ecm.context.IElement;
+import cj.studio.ecm.context.IProperty;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -14,13 +16,13 @@ public class CuratorPlugin implements IChipPlugin {
 
     @Override
     public void load(IAssemblyContext ctx, IElement args) {
-        String retryIntervalMs = args.getNode("retryIntervalMs") == null ? "5000" : args.getNode("retryIntervalMs").getName();
-        String connectString = args.getNode("connectString") == null ? "" : args.getNode("connectString").getName();
-        String sessionTimeoutMs = args.getNode("sessionTimeoutMs") == null ? "60000" : args.getNode("sessionTimeoutMs").getName();
-        String connectionTimeoutMs = args.getNode("connectionTimeoutMs") == null ? "60000" : args.getNode("connectionTimeoutMs").getName();
-        String namespace = args.getNode("namespace") == null ? "/" : args.getNode("namespace").getName();
+        String retryIntervalMs = args.getNode("retryIntervalMs") == null ? "5000" : ((IProperty) args.getNode("retryIntervalMs")).getValue().getName();
+        String connectString = args.getNode("connectString") == null ? "" : ((IProperty) args.getNode("connectString")).getValue().getName();
+        String sessionTimeoutMs = args.getNode("sessionTimeoutMs") == null ? "60000" : ((IProperty) args.getNode("sessionTimeoutMs")).getValue().getName();
+        String connectionTimeoutMs = args.getNode("connectionTimeoutMs") == null ? "60000" : ((IProperty) args.getNode("connectionTimeoutMs")).getValue().getName();
+        String namespace = args.getNode("namespace") == null ? "" : ((IProperty) args.getNode("namespace")).getValue().getName();
         RetryPolicy retryPolicy = new RetryForever(Integer.valueOf(retryIntervalMs));
-        CuratorFramework client =
+        client =
                 CuratorFrameworkFactory.builder()
                         .connectString(connectString)
                         .sessionTimeoutMs(Integer.valueOf(sessionTimeoutMs))
@@ -39,6 +41,9 @@ public class CuratorPlugin implements IChipPlugin {
 
     @Override
     public Object getService(String serviceId) {
-        return client;
+        if ("framework".equals(serviceId)) {
+            return client;
+        }
+        return new EcmException("格式错误，应用：插件名.framework ，例：curator.framework");
     }
 }
